99635a4014849af94059c7e962da5dbdb93f52c6
abhijit8918 <abhijitpatra8918@gmail.com>
2025-05-29 17:11:31 +0530

    Add files via upload

---
+++ Task 2.py
@@ -0,0 +1,4 @@
+# Personalise greeting
+name= input('Enter your first name: ')
+surname= input('Enter your last name: ')
+print('Hello, ' + name + ' '+ surname +'!' + ' Welcome to the python program.')
---
+++ Task1.py
@@ -0,0 +1,14 @@
+# Input the Numbers
+a= input('Enter the first number: ')
+b= input('Enter the second number: ')
+a= float(a)
+b= float(b)
+
+# Addition
+print('Addition : ', a+b)
+# Subtraction
+print('Subtraction : ', a-b)
+# Multiplication
+print('Multiplication: ', a*b)
+# Division
+print('Division: ', a/b)
